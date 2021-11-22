113e93e57a22c01eb0f84072452c25372795815f
Vasyl Leskiv <70316471+vasyl-leskiv@users.noreply.github.com>
2021-11-22 23:19:16 +0200

    upd

--- kube-secret.tf
+++ kube-secret.tf
@@ -1,29 +1,4 @@
-variable "name_secretid" {
-  description = "secret id: name of the key to be used in kubernetes secret"
-  type        = string
-  default     = "secretid"
-}
-
-variable "name_roleid" {
-  description = "role id: name of the key to be used in kubernetes secret"
-  type        = string
-  default     = "roleid"
-}
-variable "template_path" {
-  description = ""
-  type        = string
-  default     = ""
-}
-
-variable "vault_addr" {
-  description = ""
-  type        = string
-  default     = "http://localhost:8200"
-}
-
 locals {
-  secret_ns   = "default"
-  secret_name = "mysecret"
   value_roleid = vault_approle_auth_backend_role.this.role_id
   value_secretid = (
     length(vault_approle_auth_backend_role_secret_id.this) > 0 ?
@@ -34,8 +9,8 @@ locals {
 
 resource "kubernetes_secret" "my_secret" {
   metadata {
-    name      = local.secret_name
-    namespace = local.secret_ns
+    name      = var.secret_name
+    namespace = var.secret_ns
   }
   type = "Opaque"
   data = {
